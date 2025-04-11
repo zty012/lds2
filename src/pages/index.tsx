b4811187ce0e29ac35776a51dc4555c8ea4aa336
zty012 <me@zty012.de>
2025-04-11 21:37:58 +0800

    ✨ 添加首页

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
+import { getTauriVersion, getVersion } from "@tauri-apps/api/app";
+import { Flex, Typography } from "antd";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <>home</>;
+  const [version, setVersion] = useState("");
+  const [tauriVersion, setTauriVersion] = useState("");
+
+  useEffect(() => {
+    getVersion().then((v) => {
+      setVersion(v);
+    });
+    getTauriVersion().then((v) => {
+      setTauriVersion(v);
+    });
+  }, []);
+
+  return (
+    <Flex
+      justify="center"
+      align="center"
+      vertical
+      gap={4}
+      style={{ height: "100%" }}
+    >
+      <Typography.Title>lds 的精神制裁</Typography.Title>
+      <Typography.Paragraph>应用版本: {version}</Typography.Paragraph>
+      <Typography.Paragraph>框架版本: {tauriVersion}</Typography.Paragraph>
+    </Flex>
+  );
 }
